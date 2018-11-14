7cc15acac6918c7d65c064984b032cbdfb1bd757
LKS007 <leon99_21@mail.ru>
2018-11-15 02:12:03 +0300

    Add login, signup for user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(login: params[:session][:login])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to '/admin'
+    else
+      redirect_to '/login'
+    end 
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/'
+  end
+end
